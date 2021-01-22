a5ca09f0f1c9cef1bb4b9882200cde2da9d78beb
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 17:44:14 +0530

    ADDED: Both length of the lines are equal

--- Cartesion.java
+++ Cartesion.java
@@ -3,14 +3,14 @@ public class Cartesion {
 	{
 		int x1,y1,x2,y2,x3,y3,x4,y4; //cordinated
 		double length1, length2;
-		x1=4;
-		y1=5;
-		x2=5;
-		y2=6;
-		x3=7;
-		y3=8;
-		x4=6;
-		y4=3;
+		x1=0;
+		y1=0;
+		x2=0;
+		y2=0;
+		x3=0;
+		y3=0;
+		x4=0;
+		y4=0;
 		
 		//calculated lenth of two lines 
 		length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
@@ -18,5 +18,10 @@ public class Cartesion {
 
 		System.out.println("the length of line1 is:"+length1);
 		System.out.println("the length of line2 is:"+length2);
+
+		if( length1 == length2 )
+		{
+			System.out.println("both line length are equal");
+		}
 	}
 }
