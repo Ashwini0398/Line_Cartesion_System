e336583e98cc1e7a188217ba60c97d19ea267604
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 17:31:24 +0530

    ADDED: Cartesion system

---
+++ Cartesion.java
@@ -0,0 +1,21 @@
+public class Cartesion {
+	public static void main(String args[])
+	{
+		int x1,y1,x2,y2,x3,y3,x4,y4;
+		double length1, length2;
+		x1=4;
+		y1=5;
+		x2=5;
+		y2=6;
+		x3=7;
+		y3=8;
+		x4=6;
+		y4=3;
+
+		length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+		length2 = Math.sqrt(Math.pow(x3-x4,2) + Math.pow(y3-y4,2));
+
+		System.out.println("the length of line1 is:"+length1);
+		System.out.println("the length of line2 is:"+length2);
+	}
+}
