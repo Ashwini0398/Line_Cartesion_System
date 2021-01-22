3d85f404d7dc13f93b93eff1842e5c0803c368cb
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 19:31:21 +0530

    ADDED: Comparision

--- Cartesion.java
+++ Cartesion.java
@@ -1,36 +1,45 @@
-public class Cartesion {
-	public static void main(String args[])
-	{
-		int x1,y1,x2,y2,x3,y3,x4,y4; //cordinated
-		double length1, length2;
-		x1=3;
-		y1=4;
-		x2=5;
-		y2=6;
-		x3=7;
-		y3=4;
-		x4=6;
-		y4=7;
-		
-		//calculated lenth of two lines 
-		length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-		length2 = Math.sqrt(Math.pow(x3-x4,2) + Math.pow(y3-y4,2));
+public class Cartesion
+{
+                public static final int x1=3;
+                public static final int y1=4;
+                public static final int x2=5;
+                public static final int y2=6;
+                public static final int x3=7;
+                public static final int y3=4;
+                public static final int x4=6;
+                public static final int y4=7;
 
-		System.out.println("the length of line1 is:"+length1);
-		System.out.println("the length of line2 is:"+length2);
+        public static void main(String args[])
+        {
+                double length1, length2;
 
-		if( length1 == length2 )
-		{
-			System.out.println("both line length are equal");
-		}
-		else if ( length1 > length2 )
-		{
-			System.out.println("length of line 1 is greater");	
-		}
-		else
-		{
-			System.out.println(" length of line 2  is greater");
-		}
 
-	}
+                //calculated lenth of two lines
+                length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+                length2 = Math.sqrt(Math.pow(x3-x4,2) + Math.pow(y3-y4,2));
+
+                System.out.println("the length of line1 is:"+length1);
+                System.out.println("the length of line2 is:"+length2);
+
+                String str1=String.valueOf(length1);
+                String str2=String.valueOf(length2);
+
+                double var = str1.compareTo(str2);
+
+
+
+                if( var == 0 )
+                {
+                        System.out.println("both line length are equal");
+                }
+                else if ( var > 0 )
+                {
+                        System.out.println("length of line 1 is greater");
+                }
+                else
+                {
+                        System.out.println(" length of line 2  is greater");
+                }
+
+        }
 }
