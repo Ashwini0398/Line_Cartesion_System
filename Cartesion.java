1378a8020204fac92a16ad8a1512e328d3267249
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 17:47:58 +0530

    MODIFIED: Compare two line length

--- Cartesion.java
+++ Cartesion.java
@@ -3,14 +3,14 @@ public class Cartesion {
 	{
 		int x1,y1,x2,y2,x3,y3,x4,y4; //cordinated
 		double length1, length2;
-		x1=0;
-		y1=0;
-		x2=0;
-		y2=0;
-		x3=0;
-		y3=0;
-		x4=0;
-		y4=0;
+		x1=3;
+		y1=4;
+		x2=5;
+		y2=6;
+		x3=7;
+		y3=4;
+		x4=6;
+		y4=7;
 		
 		//calculated lenth of two lines 
 		length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
@@ -23,5 +23,14 @@ public class Cartesion {
 		{
 			System.out.println("both line length are equal");
 		}
+		else if ( length1 > length2 )
+		{
+			System.out.println("length of line 1 is greater");	
+		}
+		else
+		{
+			System.out.println(" length of line 2  is greater");
+		}
+
 	}
 }
